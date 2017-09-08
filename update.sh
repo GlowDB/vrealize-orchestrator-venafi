3f7659eb7a3293dd25b67ffd74aabb86c0dac401
Justin Coole <31752817+jcoole@users.noreply.github.com>
2017-09-07 19:59:49 -0700

    Delete update.sh

--- update.sh
+++
@@ -1,2 +0,0 @@
-mvn versions:set -DnewVersion=$1-SNAPSHOT -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true
-mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true
